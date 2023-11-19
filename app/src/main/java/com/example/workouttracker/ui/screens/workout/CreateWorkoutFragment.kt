3e89e4c4c15d30542a4d1c77a309f58bf02efe1a
SirClashin1 <willem.coding@gmail.com>
2023-11-19 21:45:15 +0200

    resolve merge conflicts

--- app/src/main/java/com/example/workouttracker/ui/screens/workout/CreateWorkoutFragment.kt
+++ app/src/main/java/com/example/workouttracker/ui/screens/workout/CreateWorkoutFragment.kt
@@ -2,6 +2,7 @@ package com.example.workouttracker.ui.screens.workout
 
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,15 +10,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.workouttracker.data.ExerciseSet
 import com.example.workouttracker.ui.screens.workout.WorkoutViewModel
 import com.example.workouttracker.ui.theme.WorkoutTrackerTheme
 import kotlin.math.log
@@ -47,13 +55,32 @@ fun CreateWorkoutScreen(controller : NavController) {
 @Composable
 fun CreateWorkoutContent(controller: NavController) {
     val workoutViewModel: WorkoutViewModel = viewModel(LocalContext.current as ComponentActivity)
-    val exercises = workoutViewModel.currentWorkoutExercises
-    println("test "+exercises.size)
+    val currentExercises = workoutViewModel.currentWorkoutExercises
+    val exerciseSets = workoutViewModel.exerciseSets
+
     LazyColumn {
-        items(exercises) { exercise ->
-            Text(exercise.name)
+        items(currentExercises) { exercise ->
+            Card(modifier = Modifier.padding(8.dp)) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(exercise.name, style = MaterialTheme.typography.h6)
+
+                    val sets = exerciseSets[exercise.id] ?: mutableListOf()
+                    if (sets.isEmpty()) {
+                        workoutViewModel.addSetToAttempt(exercise.id)
+                    } else {
+                        sets.forEachIndexed { index, set ->
+                            SetInputRow(setDetails = set) { weight, reps ->
+                                workoutViewModel.updateSet(exercise.id, set.setId, weight, reps)
+                                workoutViewModel.onSetDetailsUpdated(exercise.id)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.BottomCenter
@@ -72,8 +99,10 @@ fun CreateWorkoutContent(controller: NavController) {
             Button(
                 onClick = {
                     controller.popBackStack()
-                    workoutViewModel.stopTimer()
-               },
+//                    workoutViewModel.saveWorkout {
+//                        controller.navigate("destination_after_saving")
+//                    }
+                },
                 colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
             ) {
                 Text("End Workout")
@@ -82,4 +111,57 @@ fun CreateWorkoutContent(controller: NavController) {
     }
 }
 
+//@Composable
+//fun ExerciseSetCard(attemptId: Int, sets: MutableList<ExerciseSet>, workoutViewModel: WorkoutViewModel) {
+//    Card(modifier = Modifier.padding(8.dp)) {
+//        Column(modifier = Modifier.padding(16.dp)) {
+//            sets.forEach { set ->
+//                SetInputRow(set) { updatedSet ->
+//                    workoutViewModel.updateSet(
+//                        attemptId,
+//                        updatedSet.setId,
+//                        updatedSet.weight,
+//                        updatedSet.reps
+//                    )
+//                }
+//            }
+//            Button(onClick = { workoutViewModel.addSetToAttempt(attemptId) }) {
+//                Text("Add Set")
+//            }
+//        }
+//    }
+//}
+
+@Composable
+fun SetInputRow(setDetails: ExerciseSet, onSetChanged: (Double, Int) -> Unit) {
+    println("test")
+    var weight by remember { mutableStateOf(setDetails.weight.toString()) }
+    var reps by remember { mutableStateOf(setDetails.reps.toString()) }
+
+    Row(modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = weight,
+            onValueChange = {
+                weight = it
+                onSetChanged(it.toDoubleOrNull() ?: 0.0, setDetails.reps)
+            },
+            label = { Text("Weight (kg)") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.weight(1f)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        OutlinedTextField(
+            value = reps,
+            onValueChange = {
+                reps = it
+                onSetChanged(setDetails.weight, it.toIntOrNull() ?: 0)
+            },
+            label = { Text("Reps") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
 
